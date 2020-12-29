aad4ea6e7f921b8312ca15872f29b247c2bc473c
Zachary Jordan <pixelwelders@gmail.com>
2020-12-29 14:11:46 -0600

    Style video.

--- src/features/course2/ItemView.js
+++ src/features/course2/ItemView.js
@@ -8,18 +8,20 @@ const ItemView = ({ item }) => {
         <p>No item.</p>
       )}
       {item && (
-        <div>
-          <p>{item.displayName}</p>
+        <>
+          {!item.playbackId && (
+            <p>No video.</p>
+          )}
           {item.playbackId && (
             <ReactPlayer
-              width={400}
-              height={300}
+              width={'100%'}
+              height={'100%'}
               style={{ border: '3px solid blue' }}
               url={`https://stream.mux.com/${item.playbackId}.m3u8`}
               controls={true}
             />
           )}
-        </div>
+        </>
       )}
     </div>
   );
