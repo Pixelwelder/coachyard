8cdfc1cca90388b965adfdd3649cff05e50919d2
Zachary Jordan <pixelwelders@gmail.com>
2021-03-31 09:59:30 -0500

    Fix bug AGAIN that prevented changing video uploads.

--- functions/items/index.js
+++ functions/items/index.js
@@ -166,28 +166,7 @@ const sendItem = async (data, context) => {
       .collection('courses').doc(courseUid)
       .collection('items').doc(itemUid);
     const itemDoc = await itemRef.get();
-    const itemData = itemDoc.data();
-    if (itemData.streamingId) {
-      // Already exists. Delete it.
-      log({ message: `Streaming exists. Deleting ${itemData.streamingId}...`, data, context });
-      const result = await fetch(
-        `https://api.mux.com/video/v1/assets/${itemData.streamingId}`,
-        {
-          method: METHODS.DELETE,
-          headers: getMuxHeaders()
-        }
-      );
-      try {
-        const json = await result.json();
-        log({ message: 'Existing streaming asset deleted.', data: json, context });
-      } catch (error) {
-        log({ message: 'Existing streaming asset could not be deleted.', data: json, context, level: 'error' });
-        console.log('------ ERROR ------');
-        console.log(error);
-        console.log(result);
-        console.log('-----------------------------------');
-      }
-    }
+    const { streamingId: oldStreamingId } = itemDoc.data();
 
     const result = await fetch(
       'https://api.mux.com/video/v1/assets',
@@ -213,11 +192,36 @@ const sendItem = async (data, context) => {
       // Also store it elsewhere for the webhook later.
       const procRef = admin.firestore().collection('mux_processing').doc(streamingId);
       const procItem = {
+        awaiting: 'processing',
         courseUid,
         itemUid,
       };
       await transaction.set(procRef, procItem);
     });
+
+    // Delete old one if necessary.
+    if (oldStreamingId) {
+      // Already exists. Delete it.
+      log({ message: `Streaming exists. Deleting ${oldStreamingId}...`, data, context });
+      const result = await fetch(
+        `https://api.mux.com/video/v1/assets/${oldStreamingId}`,
+        {
+          method: METHODS.DELETE,
+          headers: getMuxHeaders()
+        }
+      );
+      try {
+        console.log(result);
+        const json = await result.json();
+        log({ message: 'Existing streaming asset deleted.', data: json, context });
+      } catch (error) {
+        log({ message: 'Existing streaming asset could not be deleted.', data: json, context, level: 'error' });
+        console.log('------ ERROR ------');
+        console.log(error);
+        console.log(result);
+        console.log('-----------------------------------');
+      }
+    }
 
     log({ message: 'Updated database with streaming asset.', data: json, context });
     return { message: 'Done. I think.', result: json };
