005e6cd87df3ce63c74643ff06da380393121d71
Zachary Jordan <pixelwelders@gmail.com>
2021-05-15 15:34:30 -0500

    More file juggling.

--- functions/courses/index.js
+++ functions/courses/index.js
@@ -1,25 +1,11 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const fetch = require('node-fetch');
 const { log } = require('../logging');
 const { checkAuth } = require('../util/auth');
-const { getMuxHeaders } = require('../util/headers');
-const { METHODS } = require('../util/methods');
 const { tokenUpdateFromCourse, tokenFromCourse2, getChildCourseUpdate, filterCourseItem } = require('./utils');
 const { newCourse, newCourseItem, newCourseToken } = require('../data');
 const { uploadImage } = require('./images');
 const { initializePurchase } = require('./initializePurchase');
-const stripe = require('../billing/stripe');
-
-const tokenFromCourse = (course, user) => {
-  return {
-    ...tokenUpdateFromCourse(course),
-    access: 'student',
-    type: 'basic',
-    user: user.uid,
-    userDisplayName: user.displayName
-  };
-};
 
 /**
  * A simpler createCourse.
@@ -178,128 +164,12 @@ const deleteCourse = async (data, context) => {
   }
 };
 
-const onCourseUpdated = functions.firestore
-  .document('/courses/{docId}')
-  .onUpdate(async (change, context) => {
-    const { docId } = context.params;
-    log({ message: `Course ${docId} was updated.`, data: change.after.data(), context });
-
-    // Update all tokens if necessary.
-    const before = change.before.data();
-    const after = change.after.data();
-    const updateRequired = ['displayName', 'description', 'price'].find(key => before[key] !== after[key]);
-    if (!updateRequired) {
-      console.log('No update required.');
-      return;
-    }
-
-    await admin.firestore().runTransaction((async (transaction) => {
-      try {
-        const { displayName, description, price, uid } = change.after.data();
-        const tokensRef = admin.firestore()
-          .collection('tokens')
-          .where('courseUid', '==', uid)
-          .select();
-
-        const tokens = await transaction.get(tokensRef);
-        const promises = tokens.docs.map((doc) => {
-          return transaction.update(doc.ref, {
-            displayName,
-            description,
-            price
-          })
-        });
-
-        await Promise.all(promises).catch(error => {
-          log({ message: error.message, data: error, context, level: 'error' });
-        });
-      } catch (error) {
-        log({ message: error.message, data: change.after.data(), context, level: 'error' });
-      }
-    }));
-  });
-
-const onCourseDeleted = functions.firestore
-  .document('/courses/{docId}')
-  .onDelete(async (change, context) => {
-    const { docId } = context.params;
-    log({ message: `Course ${docId} was deleted.`, context });
-
-    // Delete all tokens.
-    const { uid } = change.data();
-
-    const { streamingIds } = await admin.firestore().runTransaction(async (transaction) => {
-      try {
-        const tokensRef = admin.firestore()
-          .collection('tokens')
-          .where('courseUid', '==', uid)
-        const tokens = await transaction.get(tokensRef);
-
-        const itemsRef = admin.firestore()
-          .collection('courses').doc(docId)
-          .collection('items');
-        const items = await transaction.get(itemsRef);
-
-        // Delete tokens.
-        const tokenPromises = tokens.docs.map((doc) => transaction.delete(doc.ref));
-
-        // Delete items, saving streamingIds for the last step.
-        const { promises: itemPromises, streamingIds } = items.docs.reduce((accum, doc) => {
-          const item = doc.data();
-          const newAccum = {
-            promises: [...accum.promises, transaction.delete(doc.ref)],
-            streamingIds: [...accum.streamingIds]
-          };
-
-          if (item.streamingId) newAccum.streamingIds.push(item.streamingId);
-
-          return newAccum;
-        }, { promises: [], streamingIds: [] });
-
-        await Promise.all([...tokenPromises, itemPromises]).catch(error => {
-          log({ message: error.message, data: error, context, level: 'error' });
-        });
-        log({ message: `Deleted ${tokenPromises.length} tokens and ${itemPromises.length} items.`, context });
-
-        return { streamingIds };
-      } catch (error) {
-        log({ message: error.message, data: error, context, level: 'error' });
-      }
-    });
-
-    // Now delete from streaming server.
-    const promises = streamingIds.map(async (streamingId) => {
-      const result = await fetch(
-        `https://api.mux.com/video/v1/assets/${streamingId}`,
-        {
-          method: METHODS.DELETE,
-          headers: getMuxHeaders()
-        }
-      );
-
-      // TODO No way to know.
-    });
-
-    await Promise.all(promises).catch(error => {
-      log({ message: error.message, data: error, context, level: 'error' });
-    });
-    log({ message: `Deleted ${promises.length} items from streaming server.`, context });
-  });
-
-
 module.exports = {
   // Courses
-  // createCourse: functions.https.onCall(createCourse),
   createCourse2: functions.https.onCall(createCourse2),
   updateCourse: functions.https.onCall(updateCourse),
-  // getCourse: functions.https.onCall(getCourse),
   deleteCourse: functions.https.onCall(deleteCourse),
   initializePurchase: functions.https.onCall(initializePurchase),
-  // updateCourse: functions.https.onCall(updateCourse),
-  // giveCourse: functions.https.onCall(giveCourse),
-  // getAllCourses: functions.https.onCall(getAllCourses),
-  // getCreatedCourses: functions.https.onCall(getCreatedCourses),
-  onCourseUpdated,
-  onCourseDeleted,
-  ...require('./users')
+  ...require('./users'),
+  ...require('./handlers')
 };
